94b42a9fb0ec61c72c01e64711f2b361f08ea51b
Mathijs van Mourick <matvamo@gmail.com>
2018-09-14 11:16:59 +0200

    Add solutions for module 1 of Algorithms and Data Structures in C# course

---
+++ Algorithms and Data Structures in C# course/Mod1_BubbleSort/Mod1_BubbleSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, 10, 3, 2, 4 };
+            Console.WriteLine("Before: " + string.Join(", ", nums));
+            nums = BubbleSort(nums);
+            Console.WriteLine("After: " + string.Join(", ", nums));
+        }
+
+        public static int[] BubbleSort(int[] nums)
+        {
+            bool swapped = false;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < nums.Length - 1 ; i++)
+                {
+                    if (nums[i] > nums[i + 1])
+                    {
+                        int temp = nums[i + 1];
+                        nums[i + 1] = nums[i];
+                        nums[i] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+
+            return nums;
+        }
+
+    }
+}
---
+++ Algorithms and Data Structures in C# course/Mod1_CylinderCalculator/Mod1_CylinderCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_CylinderCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The volume of a cylinder with base 3 and height 5 is {0}", CalculateVolumeCylinder(3, 5));
+        }
+
+        static double CalculateVolumeCylinder(double radius, double height)
+        {
+            return Math.PI * Math.Pow(radius, 2) * height;
+        }
+    }
+}
