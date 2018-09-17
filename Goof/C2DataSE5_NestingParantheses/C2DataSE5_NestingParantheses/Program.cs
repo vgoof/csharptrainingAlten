9ec9c334d2939405221e03caa8767260aab3a160
Goof van de Weg <goof.van.de.weg@alten.nl>
2018-09-17 09:21:01 +0200

    Added C2 data self assessment nesting parantheses

---
+++ Goof/C2DataSE5_NestingParantheses/C2DataSE5_NestingParantheses/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2DataSE5_NestingParantheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = "a(())b()";
+            int aC = Parentheses(a);
+            Console.WriteLine("ans is {0}", aC);
+
+            String b = "(()1()";
+            int bC = Parentheses(b);
+            Console.WriteLine("ans is {0}", bC);
+
+        }
+
+        public static int Parentheses(string s)
+        {
+            // Approach: go through the string and 
+            // if ( -> depth ++, 
+            // if ) 0> depth --
+            // if depth < 0 -> return maxdepth = 0; 
+            // if depth at end > 0 -> return maxdepth = 0;
+            int depth = 0;
+            int maxdepth = 0;         
+            foreach (char el in s) {
+                if (el == '(')
+                {
+                    depth++;
+                }
+                else if (el == ')')
+                {
+                    depth--;
+                }
+
+                if (depth < 0) {
+                    return 0; 
+                }
+
+                maxdepth = Math.Max(depth, maxdepth);
+
+            }
+
+            if (depth > 0)
+            {
+                return 0;
+            }
+
+
+            return maxdepth;
+        }
+
+    }
+}
