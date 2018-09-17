3d789f669984df054e1687b71a2e828ab651e2c2
Goof van de Weg <goof.van.de.weg@alten.nl>
2018-09-17 10:01:50 +0200

    Finished tutorial 2, starting self assessmentlab

---
+++ Goof/C2CollectionsTutorial2/C2CollectionsTutorial2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace C2CollectionsTutorial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable openWith = new Hashtable();
+            // Add some elements to the hash table. There are no 
+            // duplicate keys, but some of the values are duplicates.
+            openWith.Add("txt", "notepad.exe");
+            openWith.Add("bmp", "paint.exe");
+            openWith.Add("dib", "paint.exe");
+            openWith.Add("rtf", "wordpad.exe");
+            // The Add method throws an exception if the new key is 
+            // already in the hash table.
+            try
+            {
+                openWith.Add("txt", "winword.exe");
+            }
+            catch
+            {
+                Console.WriteLine("An element with Key = \"txt\" already exists.");
+            }
+            // The Item property is the default property, so you 
+            // can omit its name when accessing elements. 
+            Console.WriteLine("For key = \"rtf\", value = {0}.", openWith["rtf"]);
+
+            // The default Item property can be used to change the value
+            // associated with a key.
+            openWith["rtf"] = "winword.exe";
+            Console.WriteLine("For key = \"rtf\", value = {0}.", openWith["rtf"]);
+
+            // If a key does not exist, setting the default Item property
+            // for that key adds a new key/value pair.
+            openWith["doc"] = "winword.exe";
+
+            // ContainsKey can be used to test keys before inserting 
+            // them.
+            if (!openWith.ContainsKey("ht"))
+            {
+                openWith.Add("ht", "hypertrm.exe");
+                Console.WriteLine("Value added for key = \"ht\": {0}", openWith["ht"]);
+            }
+
+            // When you use foreach to enumerate hash table elements,
+            // the elements are retrieved as KeyValuePair objects.
+            Console.WriteLine();
+            foreach (DictionaryEntry de in openWith)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", de.Key, de.Value);
+            }
+
+        }
+    }
+}
