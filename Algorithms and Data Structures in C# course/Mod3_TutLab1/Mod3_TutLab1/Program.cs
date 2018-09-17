c39c89faa2a89a0c57bce602b8a225fef5a167f3
Mathijs van Mourick <mathijs.van.mourick@alten.nl>
2018-09-17 13:23:08 +0200

    Add Module 3 tutorial labs

---
+++ Algorithms and Data Structures in C# course/Mod3_TutLab1/Mod3_TutLab1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace Mod3_TutLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myArrList = new ArrayList();
+
+            myArrList.Add("First Item");
+            myArrList.Add("Third Item");
+            myArrList.Add("Second Item");
+
+            myArrList.Sort();
+
+            int itemIndex = myArrList.IndexOf("Third Item");
+
+            foreach(object obj in myArrList)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+
+            Console.WriteLine($"Third Item is at index {itemIndex}");
+
+
+
+        }
+    }
+}
---
+++ Algorithms and Data Structures in C# course/Mod3_TutLab2/Mod3_TutLab2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace Mod3_TutLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable openWith = new Hashtable();
+
+            openWith.Add("txt", "notepad.exe");
+            openWith.Add("bmp", "paint.exe");
+            openWith.Add("dib", "paint.exe");
+            openWith.Add("rtf", "wordpad.exe");
+
+
+            try
+            {
+                openWith.Add("txt", "winword.exe");
+            }
+            catch
+            {
+                Console.WriteLine("An element with Key = \"txt\" already exists.");
+            }
+
+            openWith["rtf"] = "winword.exe";
+            Console.WriteLine("For key = \"rtf\", value = {0}.", openWith["rtf"]);
+
+            openWith["doc"] = "winword.exe";
+
+            if (!openWith.ContainsKey("ht"))
+            {
+                openWith.Add("ht", "hypertrm.exe");
+                Console.WriteLine("Value added for key = \"ht\": {0}", openWith["ht"]);
+            }
+
+            foreach (DictionaryEntry de in openWith)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", de.Key, de.Value);
+            }
+        }
+    }
+}
