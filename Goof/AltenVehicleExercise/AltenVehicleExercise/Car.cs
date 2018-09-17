b24e9244791f7eb01f868db59b9419cfff993965
Goof van de Weg <goof.van.de.weg@alten.nl>
2018-09-17 11:59:15 +0200

    Alten C# exercise 1.1 completed

---
+++ Goof/AltenVehicleExercise/AltenVehicleExercise/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AltenVehicleExercise
+{
+    class Car
+    {
+        private string m_brandName;
+        private string m_modelName;
+
+        public Car(string brandName, string modelName) {
+            BrandName = brandName;
+            m_modelName = modelName;
+        }
+
+        public string BrandName { get => m_brandName; set => m_brandName = value; }
+
+    }
+}
---
+++ Goof/AltenVehicleExercise/AltenVehicleExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AltenVehicleExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Volkswagen", "Polo");
+            Console.WriteLine($"My Car is a {myCar.BrandName}");
+        }
+    }
+}
