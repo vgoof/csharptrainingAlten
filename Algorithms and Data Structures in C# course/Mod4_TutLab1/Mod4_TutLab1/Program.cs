544a583cd3eeb9d0e821b43c78c3edb9aadce3fc
Mathijs van Mourick <mathijs.van.mourick@alten.nl>
2018-09-17 14:48:55 +0200

    Add Module4 tutorial lab

---
+++ Algorithms and Data Structures in C# course/Mod4_TutLab1/Mod4_TutLab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mod4_TutLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            Student stud1 = new Student("Tom", "Thumb", 12, "Computer Science");
+            Student stud2 = new Student("Fred", "Flintstone", 45, "Geology");
+            Student stud3 = new Student("Mickey", "Mouse", 35, "Animation");
+
+            students.Add(stud1);
+            students.Add(stud2);
+            students.Add(stud3);
+
+            foreach (Student stud in students)
+            {
+                Console.WriteLine(stud.FirstName);
+            }
+
+            bool exists = students.Contains<Student>(stud1);
+            Console.WriteLine(exists.ToString());
+
+            students.Remove(stud3);
+            Console.WriteLine(students.Count());
+        }
+    }
+}
---
+++ Algorithms and Data Structures in C# course/Mod4_TutLab1/Mod4_TutLab1/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod4_TutLab1
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Program { get; set; }
+
+        public Student(string fn, string ln, int age, string prog)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Age = age;
+            Program = prog;
+        }
+    }
+}
