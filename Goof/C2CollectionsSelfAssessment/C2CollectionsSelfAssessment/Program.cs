ecebeb6a0944d42bc54d8ef4cfbf0726db1421e5
Goof van de Weg <goof.van.de.weg@alten.nl>
2018-09-17 10:20:37 +0200

    Finished self assessment

--- Goof/C2CollectionsSelfAssessment/C2CollectionsSelfAssessment/Program.cs
+++ Goof/C2CollectionsSelfAssessment/C2CollectionsSelfAssessment/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Sytems.Collections; 
+using System.Collections; 
 
 namespace C2CollectionsSelfAssessment
 {
@@ -11,6 +11,40 @@ namespace C2CollectionsSelfAssessment
     {
         static void Main(string[] args)
         {
+            Student s1 = new Student("Ad", "Zo");
+            Student s2 = new Student("Bo", "Ya");
+            Student s3 = new Student("Co", "Xi");
+
+            s1.AddGrade(5);
+            s1.AddGrade(6);
+            s1.AddGrade(7);
+            s1.AddGrade(3);
+            s1.AddGrade(10);
+
+            s2.AddGrade(3);
+            s2.AddGrade(9);
+            s2.AddGrade(8);
+            s2.AddGrade(2);
+            s2.AddGrade(9);
+
+            s3.AddGrade(3);
+            s3.AddGrade(4);
+            s3.AddGrade(2);
+            s3.AddGrade(10);
+            s3.AddGrade(10);
+
+            ArrayList myList = new ArrayList( );
+            myList.Add(s1);
+            myList.Add(s2);
+            myList.Add(s3);
+            
+        
+            foreach (Student si in myList) {
+                Console.WriteLine("Student with firstname {0} and lastname {1}", si.Firstname, si.Lastname);
+            }
+
+
+
         }
     }
 }
---
+++ Goof/C2CollectionsSelfAssessment/C2CollectionsSelfAssessment/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace C2CollectionsSelfAssessment
+{
+    class Student
+    {
+        private string m_firstName;
+        private string m_lastName;
+        private Stack<int> m_grades = new Stack<int>();
+
+        public Student(string firstName, string lastName) {
+            m_firstName = firstName;
+            m_lastName = lastName;
+        }
+
+        public string Firstname { get => m_firstName;}
+        public string Lastname { get => m_lastName; }
+
+        public Stack<int> Grades { get => m_grades; }
+
+        public void AddGrade(int grade) {
+            m_grades.Push(grade);
+        }
+
+    }
+}
