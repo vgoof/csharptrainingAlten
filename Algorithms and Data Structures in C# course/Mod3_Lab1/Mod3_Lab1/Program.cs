38005ddc828a24850dcc8830c292a1e913674288
Mathijs van Mourick <mathijs.van.mourick@alten.nl>
2018-09-17 13:50:39 +0200

    Add Module 3 Lab 1

---
+++ Algorithms and Data Structures in C# course/Mod3_Lab1/Mod3_Lab1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace Mod3_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList studentList = new ArrayList();
+
+            Student s1 = new Student("John", "Doe");
+            Student s2 = new Student("Jane", "Doe");
+            Student s3 = new Student("BoJack", "Horseman");
+            s1.GradeStack.Push(6);
+            s1.GradeStack.Push(7);
+            s1.GradeStack.Push(8);
+            s1.GradeStack.Push(5);
+            s1.GradeStack.Push(9);
+
+            s2.GradeStack.Push(6);
+            s2.GradeStack.Push(7);
+            s2.GradeStack.Push(8);
+            s2.GradeStack.Push(5);
+            s2.GradeStack.Push(9);
+
+            s3.GradeStack.Push(6);
+            s3.GradeStack.Push(7);
+            s3.GradeStack.Push(8);
+            s3.GradeStack.Push(5);
+            s3.GradeStack.Push(9);
+
+            studentList.Add(s1);
+            studentList.Add(s2);
+            studentList.Add(s3);
+
+            foreach(Student s in studentList)
+            {
+                Console.WriteLine($"The student is called {s.Name} {s.LastName}");
+            }
+            
+        }
+    }
+}
---
+++ Algorithms and Data Structures in C# course/Mod3_Lab1/Mod3_Lab1/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod3_Lab1
+{
+    class Student
+    {
+        public string Name { get; }
+        public string LastName { get; }
+        public Stack GradeStack { get => gradeStack; set => gradeStack = value; }
+
+        private Stack gradeStack = new Stack();
+
+        public Student(string name, string lastname)
+        {
+            Name = name;
+            LastName = lastname;
+        }
+    }
+}
