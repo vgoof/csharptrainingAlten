2108a09ce15a322d124e3d8d93aca341f71b5f52
Mathijs van Mourick <mathijs.van.mourick@alten.nl>
2018-09-17 15:30:11 +0200

    Add module 4 lab

---
+++ Algorithms and Data Structures in C# course/Mod4_Lab1/Mod4_Lab1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mod4_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Student stud1 = new Student("Tom", "Thumb", 12, "Computer Science");
+            Student stud2 = new Student("Fred", "Flintstone", 45, "Geology");
+            Student stud3 = new Student("Mickey", "Mouse", 35, "Animation");
+
+           
+
+
+            List<Student> students = new List<Student>();
+            students.Add(stud1);
+            students.Add(stud2);
+            students.Add(stud3);
+
+
+            Random random = new Random();
+
+            foreach (Student s in students)
+            {
+                for(int i = 0 ; i < 5; i++)
+                {
+                    s.grades.Add(random.Next(1, 10));
+                }
+            }
+
+            foreach(Student s in students)
+            {
+                Console.WriteLine($"This student's name is {s.FirstName} {s.LastName}.");
+                //s.grades.Pop();
+                //s.grades.Push(10);
+                Console.WriteLine($"These are his/her grades : {string.Join(", ", s.grades)}.");
+                
+            }
+
+        }
+    }
+}
---
+++ Algorithms and Data Structures in C# course/Mod4_Lab1/Mod4_Lab1/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod4_Lab1
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Program { get; set; }
+        public SortedSet<float> grades { get; set; }
+
+        public Student(string fn, string ln, int age, string prog)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Age = age;
+            Program = prog;
+            grades = new SortedSet<float>();
+        }
+    }
+}
