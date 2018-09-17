d450d406663814c78bbb6b3b6c87b46d9a51e055
Goof van de Weg <goof.van.de.weg@alten.nl>
2018-09-17 09:52:08 +0200

    Completed tutorial 1

--- Goof/C2CollectionsTutorial1/C2CollectionsTutorial1/Program.cs
+++ Goof/C2CollectionsTutorial1/C2CollectionsTutorial1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections; // needed for arraylist
 
 namespace C2CollectionsTutorial1
 {
@@ -10,6 +11,32 @@ namespace C2CollectionsTutorial1
     {
         static void Main(string[] args)
         {
+            ArrayList myArrList = new ArrayList();
+            myArrList.Add("First Item");
+            myArrList.Add(3);
+            myArrList.Add(4.5);
+
+            foreach (object obj in myArrList)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+
+            // myArrList.Sort(); // this will throw an exception because it cannot sort objects of different type
+
+            ArrayList newArrList = new ArrayList();
+            newArrList.Add("First Item");
+            newArrList.Add("Third Item");
+            newArrList.Add("Second Item");
+            newArrList.Sort();
+            int itemIndex = newArrList.IndexOf("Third Item");
+
+            foreach (object obj in newArrList)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Third Item is at index {itemIndex}.");
+
         }
     }
 }
