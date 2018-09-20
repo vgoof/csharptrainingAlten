d2a744ab1316e45e73bbe1a960e82c2711b73a9c
Mathijs van Mourick <mathijs.van.mourick@alten.nl>
2018-09-20 11:00:52 +0200

    Instantiate Cars in Program

--- AltenExercise/AltenExercise/Program.cs
+++ AltenExercise/AltenExercise/Program.cs
@@ -7,12 +7,37 @@ namespace AltenExercise
         static void Main(string[] args)
         {
 
-            CarBuilder cb = new CarBuilder();
-
-            Car vw = cb.CreateCarWithBrandName("Volkswagen")
+            Car vw = new CarBuilder()
+                .CreateCarWithBrandName("Volkswagen")
                 .WithModelName("Up!")
-                .InstallEngine(new Engine(120, 200))
+                .InstallEngine(new Engine(59, 95, EngineType.Petrol))
                 .Build();
+
+            Car peugeot = new CarBuilder()
+                .CreateCarWithBrandName("Peugeot")
+                .WithModelName("308")
+                .InstallEngine(new Engine(128, 225, EngineType.Petrol))
+                .Build();
+
+            Car ford = new CarBuilder()
+                .CreateCarWithBrandName("Ford")
+                .WithModelName("Focus")
+                .InstallEngine(new Engine(123, 201, EngineType.Diesel))
+                .Build();
+
+            Car honda = new CarBuilder()
+                .CreateCarWithBrandName("Honda")
+                .WithModelName("Civic")
+                .InstallEngine(new Engine(90, 130, EngineType.Hybrid))
+                .Build();
+
+            Car lexus = new CarBuilder()
+                .CreateCarWithBrandName("Lexus")
+                .WithModelName("CT200h")
+                .InstallEngine(new Engine(125, 210, EngineType.Hybrid))
+                .Build();
+
+
 
         }
     }
